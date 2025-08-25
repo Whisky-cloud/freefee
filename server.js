03c550774f15a2d9c5914a093385b405c2a5f9c3
Whisky-cloud <p2pm751l@gmail.com>
2025-08-25 12:25:56 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,2 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("Hello World"));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("GOOGLE_PROJECT_ID:", process.env.GOOGLE_PROJECT_ID ? "OK" : "NOT SET");
+console.log("GOOGLE_CREDENTIALS:", process.env.GOOGLE_CREDENTIALS ? "OK" : "NOT SET");
